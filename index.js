3b716d4a3b7909c4aa2164d338b2be9665735699
Ahmed Abdullah <Ahmad.Qadir@outlook.com>
2022-01-03 15:23:45 +0300

    dasd

--- index.js
+++ index.js
@@ -13,8 +13,18 @@ const swaggerUi = require("swagger-ui-express");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors())
-app.options('*', cors())
+var allowlist = ['https://accounting-ms.herokuapp.com/Products/Parser']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.use(cors(corsOptionsDelegate))
 
 const fs = require('fs')
 const csv = require('csv-parser')
