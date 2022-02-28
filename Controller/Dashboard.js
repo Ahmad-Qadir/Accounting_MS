8ea0df89c2d403df5a0d2e59d86067839504d559
Ahmed Abdullah <Ahmad.Qadir@outlook.com>
2022-02-28 11:53:55 +0300

    check list added on tomarakan

--- Controller/Dashboard.js
+++ Controller/Dashboard.js
@@ -15,6 +15,7 @@ const TrailerCollection = require('../models/Trailers');
 const EmployeeClass = require('../models/Employee');
 const CompanyCollection = require('../models/Companies');
 const ItemUnitCollection = require('../models/ItemUnit');
+const DailyCollection = require('../models/Daily_Task');
 
 const address = process.env.address
 
@@ -57,10 +58,75 @@ exports.allowIfLoggedin = async (req, res, next) => {
 //Add New Product UI
 exports.GetAllMyIncome = async (req, res, next) => {
     try {
-        const Invoices = await RecordsCollection.find({}).populate('productID').sort({'createdAt':1,"manufacturerCompany":1});
+        const Invoices = await RecordsCollection.find({}).populate('productID').sort({ 'createdAt': 1, "manufacturerCompany": 1 });
+
+
+        const Daily = await DailyCollection.aggregate(
+            [
+                {
+                    $group: {
+                        _id: { moneyType: "$moneyType" },
+                        amount: { $sum: "$money" },
+                    },
+
+                },
+            ]);
+
+        const ReturnedMoney = await RecordsCollection.aggregate(
+            [
+                {
+                    $group: {
+                        _id: { Compenstate: "$Compenstate" },
+                        amount: { $sum: "$sellPrice" },
+                    },
+
+                },
+            ]);
+
+        const TotalExpenses = await RecordsCollection.aggregate(
+            [
+                {
+                    $group: {
+                        _id: null,
+                        amount: { $sum: "$totalPrice" },
+                        items: {
+                            $push: { oldDebut: "$oldDebut", discount: "$discount", prepaid: "$prepaid", recordCode: '$recordCode', softdelete: "$softdelete", trailerNumber: "$trailerNumber", productID: "$productID", cutomerID: "$cutomerID", createdAt: "$createdAt", moneyStatus: "$moneyStatus", status: "$status", totalPrice: "$totalPrice", totalQuantity: "$totalQuantity", addedBy: "$addedBy", sellPrice: "$sellPrice" },
+                        },
+                    },
+
+                },
+                {
+                    $match: {
+                        "items.status": "New Trailer",
+                    },
+                }
+            ]);
+
+        const Debtors = await ProfileCollection.aggregate(
+            [
+                {
+                    $group: {
+                        _id: null,
+                        amount: { $sum: "$remainedbalance" },
+                    },
+
+                },
+                // {
+                //     $match: {
+                //         softdelete: false,
+                //         remainedbalance: { $ne: 0 },
+                //     },
+                // }
+            ]);
+
+        console.log(TotalExpenses)
         res.render('Dashboard/Incomes.pug', {
             title: "تۆمارەکان",
             records: Invoices,
+            daily: Daily,
+            returnedmoney: ReturnedMoney,
+            debtors: Debtors,
+            expoenses:TotalExpenses
         })
 
     } catch (error) {
