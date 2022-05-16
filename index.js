f48a220fd716d23247c4a725e85d50a4e9b10696
Ahmed Abdullah <Ahmad.Qadir@outlook.com>
2022-05-16 06:09:09 +0300

    changes added

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 //Express Setter
 require('events').EventEmitter.defaultMaxListeners = Infinity
+app.engine('pug', require('pug').__express)
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.locals.basedir = 'views';
